c2602f2b56c052e5c36ff5d330f5bba0c43bce7f
kevin <kevin@C>
2023-08-31 20:50:09 -0600

    Agregar archivos de proyecto.

---
+++ recursivo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FibonacciRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de la Secuencia de Fibonacci");
+            Console.Write("Ingrese el valor de n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            long result = Fibonacci(n);
+            Console.WriteLine($"El término Fibonacci en la posición {n} es: {result}");
+        }
+
+        static long Fibonacci(int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
